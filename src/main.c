d32ac03737b5418a498f8b299ab74c16e29cb447
sebastiandine <sebastiandine91@gmail.com>
2017-06-16 12:51:03 +0200

    Second paddle added. Basic ball movement and hit detection with paddles.

--- src/main.c
+++ src/main.c
@@ -5,7 +5,16 @@
 
 #pragma bss-name(push, "ZEROPAGE")
 unsigned char oam_offset;
+unsigned char player1_pos_x;
 unsigned char player1_pos_y;
+unsigned char player1_score;
+unsigned char player2_pos_x;
+unsigned char player2_pos_y;
+unsigned char player2_score;
+unsigned char flag_score;
+unsigned char ball_pos_x;
+unsigned char ball_pos_y;
+unsigned char ball_dir; //1 = right, 2 = left
 #pragma bss-name(pop)
 
 const unsigned char bg_palette[16]  =
@@ -23,7 +32,11 @@ const unsigned char sprite_palette[16]  =
 
 void main(void){
     oam_offset = 0;
-    player1_pos_y = 60;
+
+    player1_score = 0;
+    player2_score = 0;
+    ball_dir = 1;
+
     ppu_turn_all_off();
     set_bg_palette(bg_palette);
     set_sprite_palette(sprite_palette);
@@ -34,37 +47,97 @@ void main(void){
     wait_Vblank();
     ppu_turn_all_on();
 
+    while(1) {          //match-loop
+        player1_pos_x = 10;
+        player1_pos_y = 60;
+        player2_pos_x = 246;
+        player2_pos_y = 60;
+        ball_pos_y = 68;
+        ball_pos_x = 120;
 
+        while (1) {     //score-loop
+            wait_until_nmi();
 
-	while(1){
-        wait_until_nmi();
-
-        //- genügt nicht der ASM code?
-        OAM_ADDRESS = 0;
-        OAM_DMA = 2; // push all the sprite data from the ram at 200-2ff to the sprite memory
-        PPU_CTRL = 0x90; // screen is on, NMIs on
-        PPU_MASK = 0x1e;
-        ppu_reset_scroll();
-        //- genügt nicht der ASM code?
-
-        oam_offset = 0;
-        oam_offset = ppu_load_sprite_to_oam(32,player1_pos_y,0x02,SPRITE_ATTR(0,0,0,0),oam_offset);
-        oam_offset = ppu_load_sprite_to_oam(32,player1_pos_y+8,0x04,SPRITE_ATTR(0,0,0,0),oam_offset);
-        oam_offset = ppu_load_sprite_to_oam(32,player1_pos_y+16,0x03,SPRITE_ATTR(0,0,0,0),oam_offset);
-
-        get_controller_input();
-
-        if(gamepad_1&DIR_UP){
-            if(player1_pos_y > 32){
-                --player1_pos_y;
+            /* LOGIC */
+            /* Score detection */
+            if(ball_pos_x < (player1_pos_x+8)){
+                break;
             }
-        }
-        if(gamepad_1&DIR_DOWN){
-            if(player1_pos_y < 198) {
-                ++player1_pos_y;
+            if(ball_pos_x > (player2_pos_x)){
+                break;
             }
+
+            /* paddle-hit detection */
+            if (ball_dir == 1) {
+                ++ball_pos_x;
+                if (ball_pos_x == (player2_pos_x - 8)) {
+                    if ((ball_pos_y + 8) > player2_pos_y) {           /* check if lowest point of ball is higher than highest
+                                                                   point of paddle */
+                        if (ball_pos_y < (player2_pos_y + 24)) {        /* check if if highest point of all is lower than lowest
+ *                                                                 point of paddle */
+                            ball_dir = 2;
+                        }
+
+                    }
+                }
+            }
+            if (ball_dir == 2) {
+                --ball_pos_x;
+                if (ball_pos_x == (player1_pos_x + 8)) {
+                    if ((ball_pos_y + 8) > player1_pos_y) {
+                        if (ball_pos_y < (player1_pos_y + 24)) {
+                            ball_dir = 1;
+                        }
+                    }
+                }
+            }
+
+            /* RENDER */
+            oam_offset = 0;
+            //player1
+            oam_offset = ppu_load_sprite_to_oam(player1_pos_x, player1_pos_y, 0x02, SPRITE_ATTR(0, 0, 0, 0),
+                                                oam_offset);
+            oam_offset = ppu_load_sprite_to_oam(player1_pos_x, player1_pos_y + 8, 0x04, SPRITE_ATTR(0, 0, 0, 0),
+                                                oam_offset);
+            oam_offset = ppu_load_sprite_to_oam(player1_pos_x, player1_pos_y + 16, 0x03, SPRITE_ATTR(0, 0, 0, 0),
+                                                oam_offset);
+            //player2
+            oam_offset = ppu_load_sprite_to_oam(player2_pos_x, player2_pos_y, 0x02, SPRITE_ATTR(0, 1, 0, 0),
+                                                oam_offset);
+            oam_offset = ppu_load_sprite_to_oam(player2_pos_x, player2_pos_y + 8, 0x04, SPRITE_ATTR(0, 1, 0, 0),
+                                                oam_offset);
+            oam_offset = ppu_load_sprite_to_oam(player2_pos_x, player2_pos_y + 16, 0x03, SPRITE_ATTR(0, 1, 0, 0),
+                                                oam_offset);
+            //ball
+            oam_offset = ppu_load_sprite_to_oam(ball_pos_x, ball_pos_y, 0x01, SPRITE_ATTR(0, 0, 0, 0), oam_offset);
+
+
+            /* INPUT */
+            get_controller_input();
+
+            if (gamepad_1 & DIR_UP) {
+                if (player1_pos_y > 32) {
+                    --player1_pos_y;
+                }
+            }
+            if (gamepad_1 & DIR_DOWN) {
+                if (player1_pos_y < 198) {
+                    ++player1_pos_y;
+                }
+            }
+
+            if (gamepad_2 & DIR_UP) {
+                if (player2_pos_y > 32) {
+                    --player2_pos_y;
+                }
+            }
+            if (gamepad_2 & DIR_DOWN) {
+                if (player2_pos_y < 198) {
+                    ++player2_pos_y;
+                }
+            }
+
+
         }
-
-
-	}
+    }
 }
